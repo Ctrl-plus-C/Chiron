038e0e37eb45989bb934bb388c7ca03b3c792d69
Anupam Dagar <dagaranupam@gmail.com>
2019-01-27 06:28:59 +0530

    fix navbar

--- drug/urls.py
+++ drug/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.home, name="home"),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'api/nutrient',views.NutrientsApi.as_view() ,name='Nutrient'),
-    url(r'^selfdiary/$', views.selfdiary, name="selfdiary"),
+    url(r'^selfdiary/$', views.selfdiary, name="selfdiarya"),
     url(r'api/heart_rate',views.HeartRateApi.as_view() ,name='Heart Rate'),
     url(r'api/parse', views.ParseD.as_view(), name='parse'),
     url(r'api/condition', views.Condition.as_view(), name='condition'),
